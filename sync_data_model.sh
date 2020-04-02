25331245281af7442a7b5560dbb1132b86f1ff30
Jun Chen <junchensz@foxmail.com>
2020-04-02 16:55:06 +0800

    update report files

--- sync_data_model.sh
+++ sync_data_model.sh
@@ -1,1 +1,1 @@
-newman run 022_wecube_sync_model.postman_collection.json -e $1 --disable-unicode --reporters cli,htmlextra --reporter-htmlextra-export "newman/wecube_register_plugins.html"
+newman run 022_wecube_sync_model.postman_collection.json -e $1 --disable-unicode --reporters cli,htmlextra --reporter-htmlextra-export "newman/sync_model.html"
